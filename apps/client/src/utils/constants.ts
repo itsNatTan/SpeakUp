b4131fbe9bde63743c78ac2bcfbf85fbe2eeefeb
Nathanael Mark Tan <nathanaelmark.tan@gmail.com>
2025-09-12 14:29:22 +0800

    Change default ip

--- apps/client/src/utils/constants.ts
+++ apps/client/src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const MIMETYPE = 'audio/webm; codecs=opus';
 
 export const SERVER_HOST: string =
-  import.meta.env.VITE_SERVER_HOST ?? '172.25.76.157:8443';
+  import.meta.env.VITE_SERVER_HOST ?? 'speakupfyp.duckdns.org';
 
 export const SERVER_PROTOCOL: string =
   import.meta.env.VITE_SERVER_PROTOCOL ?? 'https';
